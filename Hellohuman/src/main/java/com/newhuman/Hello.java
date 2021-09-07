0b4cfc3fb82e21a6e94a8270804e6371b69d1bc9
Isaac Soto <73193572+isaacSoto10@users.noreply.github.com>
2021-09-06 22:48:03 -0500

    Add files via upload

---
+++ Hellohuman/src/main/java/com/newhuman/Hello.java
@@ -0,0 +1,20 @@
+package com.newhuman;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class Hello {
+	@RequestMapping("/")
+	public String render1(@RequestParam(value="firstName",required=false, defaultValue="Human")String firstName,@RequestParam(value="lastName", required=false, defaultValue="Humanson")String lastName,Model model) {
+		
+			model.addAttribute("firstName",firstName);
+			model.addAttribute("lastName", lastName);
+		
+		
+		return "index.jsp";
+	}
+}
